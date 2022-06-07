d5a64d3b858f22d57de29d86ebe683597c205f7c
LeandroAgustinFernandez <leandroa.fernandez@gmail.com>
2022-06-07 16:29:48 -0300

    Create basic structure, install and config webpack

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    publicPath: '/',
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
+
+// npm install --save-dev webpack webpack-cli
+// npm install --save-dev style-loader css-loader
+// npm install --save-dev html-webpack-plugin
+// npm install --save-dev express webpack-dev-middleware
